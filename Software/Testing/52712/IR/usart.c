25666e19fca454c61396f47f9f54983b7108185e
Kyle Dillon <kjdillon3@gmail.com>
2012-05-29 20:29:35 -0700

    start implementing IR communication on xmega on master

---
+++ Software/Testing/52712/IR/usart.c
@@ -0,0 +1,28 @@
+#include <avr/io.h>
+#include "avr_compiler.h"
+#include "usart.h"
+
+void setup_usart(void)
+{
+        DDRD |= 0x02; // set the transmitt pin to be an output
+        UCSR0A = 0x02; // double transmition speed
+        UCSR0B = 0x18; // recieve enable, transmit enabled
+        UCSR0C = 0x06; // 8 bit character selected 
+        UBRR0L = 12;   // baud rate UBRR0L = (F_CLK /(16*BAUD))*2 - 1 BUAD 9600
+}
+
+void sendchar(char input)
+{
+        while(!(UCSR0A & 0x20));
+        UDR0 = input;
+}
+
+void sendstring(char* string)
+{
+        int i;
+        for(i = 0; string[i] != 0; i ++)
+        {
+                sendchar(string[i]);
+        }
+}
+
