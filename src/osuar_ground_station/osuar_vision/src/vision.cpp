c1cf96524a5f161acba885b6b03b0c8bbb51914b
Soo-Hyun Yoo <yoos117@gmail.com>
2012-07-24 18:41:00 -0700

    Minor change.

--- src/osuar_ground_station/osuar_vision/src/vision.cpp
+++ src/osuar_ground_station/osuar_vision/src/vision.cpp
@@ -3,13 +3,8 @@
 #include <iostream>
 #include <iomanip>
 
-#if (defined(_WIN32) || defined(__WIN32__) || defined(__TOS_WIN__) || defined(__WINDOWS__) || (defined(__APPLE__) & defined(__MACH__)))
-#include <cv.h>
-#include <highgui.h>
-#else
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
-#endif
 
 #include <cvblob.h>
 using namespace cvb;
