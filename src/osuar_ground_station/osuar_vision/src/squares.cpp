893354e54e0f1488407f18ff5aad3f9427697ddc
Soo-Hyun Yoo <yoos117@gmail.com>
2012-07-24 23:27:45 -0700

    Search for squarer rectangles.

--- src/osuar_ground_station/osuar_vision/src/squares.cpp
+++ src/osuar_ground_station/osuar_vision/src/squares.cpp
@@ -101,7 +101,7 @@ static void findSquares( const Mat& image, vector<vector<Point> >& squares )
                     // if cosines of all angles are small
                     // (all angles are ~90 degree) then write quandrange
                     // vertices to resultant sequence
-                    if( maxCosine < 0.3 )
+                    if( maxCosine < 0.1 )
                         squares.push_back(approx);
                 }
             }
