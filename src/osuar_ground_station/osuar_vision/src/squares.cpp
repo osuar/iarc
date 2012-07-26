5b5f6852e58e6ee61fcccc57531900c121d9b628
Soo-Hyun Yoo <yoos117@gmail.com>
2012-07-25 22:11:18 -0700

    Better default threshold values.

--- src/osuar_ground_station/osuar_vision/src/squares.cpp
+++ src/osuar_ground_station/osuar_vision/src/squares.cpp
@@ -17,8 +17,12 @@
 using namespace cv;
 
 int thresh = 50, N = 11;
+
+// Threshold for maximum cosine between angles (x100).
 int maxCosineThresh = 25;
-int maxDiffThresh = 300;
+
+// Threshold for ratio of shortest side / longest side (x100).
+int sideRatioThresh = 75;
 
 // Find colors of any hue...
 int wallHueLow  = 0;
@@ -26,7 +30,7 @@ int wallHueHigh = 179;
 
 // ...of low saturation...
 int wallSatLow  = 0;
-int wallSatHigh = 60;
+int wallSatHigh = 40;
 
 // ...ranging down to gray, but not completely dark. That is to say, white.
 int wallValLow  = 50;
@@ -193,8 +197,8 @@ int main(int argc, char** argv) {
     namedWindow("bwImage", 1);
     cvNamedWindow("control panel");
     cvCreateTrackbar("threshold",   "control panel", &thresh,      300, NULL);
-    cvCreateTrackbar("maxCosineThresh (x100)",   "control panel", &maxCosineThresh,      100, NULL);
-    cvCreateTrackbar("maxDiffThresh",   "control panel", &maxDiffThresh,      1000, NULL);
+    cvCreateTrackbar("maxCosineThresh (x100)", "control panel", &maxCosineThresh, 100, NULL);
+    cvCreateTrackbar("sideRatioThresh (x100)", "control panel", &sideRatioThresh, 100, NULL);
     cvCreateTrackbar("wallHueLow",  "control panel", &wallHueLow,  179, NULL);
     cvCreateTrackbar("wallHueHigh", "control panel", &wallHueHigh, 179, NULL);
     cvCreateTrackbar("wallSatLow",  "control panel", &wallSatLow,  255, NULL);
